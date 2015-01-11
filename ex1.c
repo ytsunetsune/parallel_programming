1bad03f8ba8e4ecbd048b55d34ac507a18705bfc
tsuneoka <tsuneoka@mizuuchi.lab.tuat.ac.jp>
2015-01-11 23:31:51 +0900

    cosme

--- ex1.c
+++ ex1.c
@@ -3,21 +3,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(){
-    int argc;       // コマンドのトークン数をカウントする
-    char cmd[80];   // 80文字のコマンド入力バッファ
-    char *cmdp;     // 作業用ポインタ
-    char *argv[10]; // トークンを入れる配列
+int main()
+{
+    int argc;        // コマンドのトークン数をカウントする
+    char cmd[80];    // 80文字のコマンド入力バッファ
+    char *cmdp;      // 作業用ポインタ
+    char *argv[10];  // トークンを入れる配列
 
     printf("コマンドを入力してください ? ");
-    if (fgets(cmd, 80, stdin) == NULL) exit(0); // コマンドの入力
-    cmd[strlen(cmd) - 1] = '\0';    // 最後の改行文字をNULL文字に変更
+    if (fgets(cmd, 80, stdin) == NULL) exit(0);  // コマンドの入力
+    cmd[strlen(cmd) - 1] = '\0';  // 最後の改行文字をNULL文字に変更
     cmdp = cmd;
-    for (argc = 0; ; argc++) {
+    for (argc = 0;; argc++) {
         if ((argv[argc] = strtok(cmdp, " ")) == NULL) break;
         cmdp = NULL;
     }
     printf("ex1は%sに変身します\n", cmd);
     execvp(argv[0], argv);
 }
-
